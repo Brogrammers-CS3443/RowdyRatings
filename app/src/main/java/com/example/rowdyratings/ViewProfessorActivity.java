f2fdd834c21e4c320c349690d8d66af38218f196
Rex-Perez <matthewrperez@yahoo.com>
2024-11-17 20:41:19 -0600

    fixed the floating action button so it now takes you to the reviews screen

--- app/src/main/java/com/example/rowdyratings/ViewProfessorActivity.java
+++ app/src/main/java/com/example/rowdyratings/ViewProfessorActivity.java
@@ -1,12 +1,17 @@
 package com.example.rowdyratings;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class ViewProfessorActivity extends AppCompatActivity {
 
@@ -15,10 +20,22 @@ public class ViewProfessorActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_view_professor);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+
+        FloatingActionButton reviewButton = findViewById(R.id.floatingActionButton);
+
+        reviewButton.setOnClickListener(new View.OnClickListener(){
+
+            public void onClick(View v){
+                launchProfessorReviewsActivity();
+            }
         });
+
+
+    }
+
+    private void launchProfessorReviewsActivity(){
+        Intent intent = new Intent(this, ProfessorReviewsActivity.class);
+        startActivity(intent);
     }
 }
